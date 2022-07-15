4fa7902f8bcac8ca0de09b216a2c2a5112fcce14
Ryabchenko.N.A <ryabchenko.n.a@topaz-atcs.com>
2022-07-15 17:35:34 +0300

    [fix] - solution problem with format

--- Flutter/api_flutter/lib/content.dart
+++ Flutter/api_flutter/lib/content.dart
@@ -90,97 +90,36 @@ class initWS extends StatelessWidget {
     var url = "http://192.168.0.101:5500//data/" + window.btoa(filename.path);
     var response = await http.get(Uri.parse(url));
     var data1;
-    //var concatenate = StringBuffer();
-    List<dynamic> datalist = [];
-    List<String> datalist2 = [];
+    List<dynamic> datalistdy = [];
 
     if (response.statusCode == 200) {
       Xml2Json xml2json = Xml2Json();
       xml2json.parse(response.body);
       var json = xml2json.toGData();
-      print('========================================');
-      //print(json);
-
       data1 = jsonDecode(json);
-      print('========================================');
       List data2 = data1['catalog']['loglist'];
-      final List<String> strs2 = data2.map((e) => e.toString()).toList();
-      datalist2 = strs2;
-
       print('========================================');
       for (int i = 0; i < data2.length; i++) {
-        datalist.add(data2[i]['log']);
+        datalistdy.add(data2[i]['log']);
         print(data2[i]['log']);
       }
     }
 
-    final String jsonObject = datalist2.join(', ');
-    //print(jsonObject);
+    final List<String> dataliststr =
+        datalistdy.map((e) => e.toString()).toList();
+    for (int i = 0; i < dataliststr.length; i++) {
+      if (dataliststr[i] == 'null') {
+        print('================null========================');
+        dataliststr.removeAt(i);
+      }
+    }
+
+    final String jsonObject = dataliststr.join(', ');
+    var jsonFinalString =
+        "[" + jsonObject.substring(0, jsonObject.length) + "]";
+    print(jsonFinalString.substring(0, 30));
     print('========================================');
-
-    /*  final myTransformer = Xml2Json();
-    final myTransformer2 = Xml2Json();
-
-    // Parse a simple XML string
-
-    myTransformer.parse(goodXmlString);
-    myTransformer2.parse(goodxmlString2);
-
-    print('XML string');
-    print(goodXmlString);
-    print('');
-    print(goodxmlString2);
-    print('');
-
-    // Transform to JSON using Badgerfish
-    var json = myTransformer.toBadgerfish();
-    var json2 = myTransformer2.toBadgerfish();
-
-    print('Badgerfish');
-    print('');
-    print(json);
-    print('');
-    print(json2);
-    print('');
-
-    // Transform to JSON using GData
-    json = myTransformer.toGData();
-    json2 = myTransformer.toGData();
-    print('GData');
-    print('');
-    print(json);
-    print('');
-    print(json2);
-    print('');
-
-    // Transform to JSON using Parker
-    json = myTransformer.toParker();
-    json2 = myTransformer2.toParker();
-    print('Parker');
-    print('');
-    print(json);
-    print('');
-    print(json2);
-    print('');
-
-    // Transform to JSON using ParkerWithAttrs
-    json = myTransformer.toParkerWithAttrs();
-    json2 = myTransformer2.toParkerWithAttrs();
-    print('ParkerWithAttrs');
-    print('');
-    print(json);
-    print('');
-    print(json2);
-    print('');
-
-    // Transform to JSON using ParkerWithAttrs
-    // A node in XML should be an array, but if there is only one element in the array,
-    // it will only be parsed into an object, so we need to specify the node as an array
-    json = myTransformer2.toParkerWithAttrs(array: ['contact']);
-    print('ParkerWithAttrs, specify the node as an array');
-    print('');
-    print(json);
- */
-    return "";
+    var json = jsonDecode(jsonFinalString);
+    return json;
   }
 }
