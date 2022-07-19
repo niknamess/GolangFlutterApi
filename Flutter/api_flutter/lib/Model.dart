79ce3d229901c107888391ba25f5c64a8f5239ba
Ryabchenko.N.A <ryabchenko.n.a@topaz-atcs.com>
2022-07-19 16:31:17 +0300

    [fix]

--- Flutter/api_flutter/lib/Model.dart
+++ Flutter/api_flutter/lib/Model.dart
@@ -32,42 +32,6 @@ class Loglist1 {
   //Loglist1.fromJSON(Map<String, dynamic> string) : log = string["log"];
 }
 
-/* class Log {
-  Log({
-    required this.moduleName,
-    required this.appPath,
-    required this.appPid,
-    required this.threadId,
-    required this.time,
-    required this.ulid,
-    required this.type,
-    required this.message,
-    required this.extMessage,
-  });
-
-  final String moduleName;
-  final String appPath;
-  final String appPid;
-  final String threadId;
-  final String time;
-  final String ulid;
-  final String type;
-  final String message;
-  final String extMessage;
-
-  factory Log.fromJson(Map<String, dynamic> json) {
-    return Log(
-        moduleName: json["module_name"],
-        appPath: json["app_path"],
-        appPid: json["app_pid"],
-        threadId: json["thread_id"],
-        time: json["time"],
-        ulid: json["ulid"],
-        type: json["type"],
-        message: json["message"],
-        extMessage: json["extMessage"]);
-  }
-} */
 class Log {
   factory Log.fromJson(Map<String, dynamic> json) {
     return Log(
